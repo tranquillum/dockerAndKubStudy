4cdad18daa13e07271052de423596c2d57c9f01a
Aleksandr Pavlov <aleksandr.pavlov.tptlive.ee>
2019-01-26 12:55:11 +0200

    add some changes in content

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            !!!!!!!!!!!!
+            Hello AWS!!!
           </a>
         </header>
       </div>
